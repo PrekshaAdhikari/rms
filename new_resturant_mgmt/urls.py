58ab08a8d9bc170123614bc5973543efd270fa72
preksha <you@example.com>
2020-03-11 01:45:18 -0400

    new folder...

---
+++ new_resturant_mgmt/urls.py
@@ -0,0 +1,29 @@
+"""new_resturant_mgmt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from main.views import HomePage
+from r_m_s.views import LoginView,Logout,Change_password
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", HomePage),
+    path("login/",LoginView),
+    path("login1/",LoginView),
+    path("logout/",Logout),
+    path("changepassword/",Change_password)
+]
